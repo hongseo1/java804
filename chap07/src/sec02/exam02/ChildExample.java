b6470d006323e2985adb494ce831c5d0d02fb1f2
hongseo1 <hongseow109@gmail.com>
2024-01-16 17:57:05 +0900

    240116

---
+++ chap07/src/sec02/exam02/ChildExample.java
@@ -0,0 +1,20 @@
+package sec02.exam02;
+
+public class ChildExample {
+
+	public static void main(String[] args) {
+		Child child = new Child();
+		child.method1();
+		child.method2();
+		child.method3();
+
+		Parent parent = child;
+		parent.method1();
+		parent.method2();
+		//parent.method3(); //(호출 불가능)
+		
+		Child child2 = (Child)parent; //강제 형변환
+		child2.method3(); //(호출 가능)
+	}
+
+}
