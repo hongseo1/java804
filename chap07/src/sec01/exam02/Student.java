73c544100fe35912ed1c2b6b5f4fbd21471aa18d
hongseo1 <hongseow109@gmail.com>
2024-01-16 15:33:30 +0900

    중간 올리기

--- chap07/src/sec01/exam02/Student.java
+++ chap07/src/sec01/exam02/Student.java
@@ -5,7 +5,7 @@ public class Student extends People {
 	
 	public Student(String name, String ssn, int studentNo) {
 		super(name, ssn); //무조건 첫번째 줄 / 부모 클래스에 파라멘트 두개가 생성자로 있기 때문에 ()안에 꼭 파라멘트 적어줘야 함.
-		// super 생략 시 부모클래스 필드를 작성해줘야 함. (예시: sec01.exam01; DmbcellPhone)
+		// super 생략 시 부모 클래스 필드를 작성해줘야 함. (예시: sec01.exam01; DmbcellPhone)
 		this.studentNo = studentNo;
 	}
 }
