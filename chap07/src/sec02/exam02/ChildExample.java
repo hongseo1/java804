78051c545f744e0cdfe72909732eb215aac44f9c
hongseo1 <hongseow109@gmail.com>
2024-01-18 15:32:14 +0900

    24-01-18 중간

--- chap07/src/sec02/exam02/ChildExample.java
+++ chap07/src/sec02/exam02/ChildExample.java
@@ -10,9 +10,11 @@ public class ChildExample {
 
 		Parent parent = child;
 		parent.method1();
-		parent.method2();
+		parent.method2(); //재정의한게 실행
 		//parent.method3(); //(호출 불가능)
 		
+		
+		//부모를 형변환 했던 거는 다시 형변환이 가능하지만, 처음부터 자식으로 형변환은 불가 
 		Child child2 = (Child)parent; //강제 형변환
 		child2.method3(); //(호출 가능)
 	}
